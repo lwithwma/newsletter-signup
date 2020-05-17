ab94cc3416ebe515d8e0cf3fdb1a19513e751ed5
Lwithwma Brahma <lwith.brah@gmail.com>
2020-05-17 23:11:25 +0530

    modified app.js file to support dynamic port

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/failure",(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-	console.log("server is listening on port 3000");
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`Server is listening on port ${process.env.PORT}`);
 })
 
